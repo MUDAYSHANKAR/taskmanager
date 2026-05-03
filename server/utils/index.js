919ad76e50720988976270a8f47e7f7e57940f07
Sai Thanush <saithanushreddy66@gmail.com>
2026-05-03 14:59:58 +0530

    Initial commit

---
+++ server/utils/index.js
@@ -0,0 +1,17 @@
+import jwt from "jsonwebtoken";
+
+const createJWT = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "1d",
+  });
+
+  const isProduction = process.env.NODE_ENV !== "development";
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: isProduction ? "none" : "lax",
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+  });
+};
+
+export default createJWT;
